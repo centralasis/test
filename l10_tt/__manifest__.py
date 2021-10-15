028d9ca7b86bd1c76cedde58e04f2a48dbd3c700
faraibernard <73079300+faraibernard@users.noreply.github.com>
2021-10-14 23:05:11 -0400

    Add files via upload

---
+++ l10_tt/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'TT - Accounting',
+    'version': '1.0',
+    'category': 'Accounting/Localizations/Account Charts',
+    'description': """
+This is the latest TT Odoo localisation necessary to run Odoo accounting for TT SME's
+=================================================================================================""",
+    'author': 'Centralasis Proactive Solutions Ltd.',
+    'website': 'https://www.centralasis.com',
+    'depends': [
+        'account',
+        'base_vat',
+    ],
+    'data': [
+        'data/l10n_tt_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account.chart.template.csv',
+        'data/account.tax.group.csv',
+        'data/account_tax_data.xml',
+        'data/account_chart_template_data.xml',
+    ],
+
+    'license': 'OPL-1',
+}
